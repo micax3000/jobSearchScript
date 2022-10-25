3fb45f4c0a5b62d955f978d68a39596fc5643ad8
micax3000 <milan.veljkovic0097@gmail.com>
2022-10-25 17:28:35 +0200

    extracted find_element_xpath method

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from pprint import pprint
 
+
+def find_element_xpath(driver, query: str):
+    return driver.find_element("xpath", query)
+
 options = Options()
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = options)
 ads = []
@@ -11,19 +15,19 @@ driver.get("https://www.joberty.rs/IT-poslovi?page=1&pageSize=100&seniority=Inte
 premiumAds = driver.find_elements("css selector", 'div.compact-job')
 commonAds = driver.find_elements("css selector", "div.mb-20")
 for premiumAd in premiumAds:
-    adInfo = premiumAd.find_element("xpath", "div/div[2]/div")
-    jobTitle = adInfo.find_element("xpath", "*/a")
-    jobProvider = adInfo.find_element("xpath", "a")
-    jobLocation = adInfo.find_element("xpath", "span/*/span[@class = 'black-link']")
+    adInfo = find_element_xpath(premiumAd, "div/div[2]/div")
+    jobTitle = find_element_xpath(adInfo, "*/a")
+    jobProvider = find_element_xpath(adInfo, "a")
+    jobLocation = find_element_xpath(adInfo, "span/*/span[@class = 'black-link']")
     ads.append([jobTitle.get_attribute("innerHTML"),
                 jobProvider.get_attribute("innerHTML"),
                 jobLocation.get_attribute("innerHTML")])
 
 for commonAd in commonAds:
-    adInfo = commonAd.find_element("xpath", "div")
-    jobTitle = adInfo.find_element("xpath", "div/div/*/a")
-    jobProvider = adInfo.find_element("xpath", "div[2]/*/a")
-    jobLocation = adInfo.find_element("xpath", "div[2]/div/span/*/span[@class = 'black-link']")
+    adInfo = find_element_xpath(commonAd,"div")
+    jobTitle = find_element_xpath(adInfo, "div/div/*/a")
+    jobProvider = find_element_xpath(adInfo, "div[2]/*/a")
+    jobLocation = find_element_xpath(adInfo, "div[2]/div/span/*/span[@class = 'black-link']")
     ads.append([jobTitle.get_attribute("innerHTML"),
                 jobProvider.get_attribute("innerHTML"),
                 jobLocation.get_attribute("innerHTML")])
